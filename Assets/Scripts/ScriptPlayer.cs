98551c8df03b706c9ae6fefa086428f57eb06269
XavierMerino01 <merinosx@campusemav.com>
2020-10-05 11:06:14 +0200

    Classe 5/10
    Fins moviment UFO

---
+++ Assets/Scripts/ScriptPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float velocitat = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y;
+
+        x= Input.GetAxis("Horizontal");
+        y= Input.GetAxis("Vertical");
+        //print("Horizontal: " + x + "/Vertical: "+y);
+        transform.Translate(x*Time.deltaTime*velocitat, y * Time.deltaTime*velocitat, 0);
+    }
+}
